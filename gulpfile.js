517b7737bca7bb17c67f0c85ca8a4551e47c1bb2
rulerofthedeities <github_rulerofthedeities@yahoo.com>
2015-12-05 12:00:24 +0100

    Implemented SASS file with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
 	concat = require('gulp-concat'),
+	sass = require('gulp-sass'),
 
 	config = {
     	bowerDir: 'bower_components/'
@@ -15,8 +16,8 @@ var gulp = require('gulp'),
 		config.bowerDir + 'font-awesome/css/font-awesome.min.css'],
 
 	vendorFonts = [
-		config.bowerDir + '/font-awesome/fonts/**.*',
-		config.bowerDir + '/bootstrap/fonts/**.*'];
+		config.bowerDir + 'font-awesome/fonts/**.*',
+		config.bowerDir + 'bootstrap/fonts/**.*'];
 
 gulp.task('vendor_scripts', function() {
 	return gulp.src(vendorJS)
@@ -30,14 +31,29 @@ gulp.task('vendor_styles', function() {
 		.pipe(gulp.dest('public/css'));
 });
 
+gulp.task('client_scss', function() {
+    return gulp.src('client/scss/**.scss')
+        .pipe(sass().on('error', sass.logError))
+		.pipe(concat('bpr.css'))
+        .pipe(gulp.dest('public/css/'));
+});
+
 gulp.task('vendor_fonts', function() {
 	return gulp.src(vendorFonts)
 		.pipe(gulp.dest('public/fonts'));
 });
 
+gulp.task('watch', function() {
+  gulp.watch('client/scss/**.scss', ['client_scss']);
+
+  //gulp.watch('client.js', ['client_scripts']);
+  //gulp.watch('client/js/*.js', ['client_scripts']);
+});
 
 gulp.task('default', [
 	'vendor_scripts', 
 	'vendor_styles', 
-	'vendor_fonts']);
+	'client_scss',
+	'vendor_fonts',
+	'watch']);
 
