71beb435c72ba247853d111c6beb7a01873efa97
rulerofthedeities <github_rulerofthedeities@yahoo.com>
2016-01-15 13:06:09 +0100

    Removed maxRecordsPerPage variable

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var express = require('express'),
 
 //config
 app.set('port', process.env.PORT || 3302);
-app.set('maxRecordsPerPage', 10);
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
